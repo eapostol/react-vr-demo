decf1e67a6804d8dda9b63c22736c6939d90bb20
Edward Apostol <ed@edapostol.com>
2018-02-26 15:32:40 -0500

    #004: example 04, added private class, text component, style

--- index.vr.js
+++ index.vr.js
@@ -3,6 +3,7 @@ import {
   AppRegistry,
   asset,
   Pano,
+	Text,
   View,
 } from 'react-vr';
 
@@ -10,13 +11,60 @@ import {
 AppRegistry - tells ReactVr what is root of project
 asset - helper fn. - looks at static-assets
 Pano - controls scene of application
+Text - adds a 'Text' Component
 View - generic VR component to wrap other components, i.e. text.
  */
 // render - basic method in React JSX, returns the native equivalent elements
 
+// create a private class . add Text import
+class SomeText extends Component {
+
+	constructor(){
+		super();
+	}
+
+	render() {
+		let textStyle = {
+			backgroundColor: '#dddddd',
+			textAlign: 'center',
+			color: '#FF0000',
+			fontSize: 0.1,
+		};
+		let someTextView = <Text style={textStyle}>{this.props.text}</Text>;
+		return (someTextView);
+	}
+}
+
 export default class ReactVRDemo2018_02 extends React.Component {
+
+	// don't forget to change the image
+	// x,y,z
+
+	constructor(props){
+		super(props);
+		this.translateVal = {
+			translate: [0,0.25,-0.5]
+		};
+
+		this.styleObj = {
+			transform:[this.translateVal],
+			layoutOrigin:[0.5, 0.5]
+		};
+	}
+
+
   render() {
-	  let theView = <View><Pano source={asset('reactvrmeetup_old.jpg')}></Pano></View>;
+
+
+
+  	let theView = <View>
+		<Pano source={asset('reactvrmeetup_old.jpg')}></Pano>
+		<View style={this.styleObj}>
+			<SomeText text="DEV.TO 2018"/>
+			<SomeText text="React VR Fun!"/>
+		</View>
+	</View>;
+
 	  return (theView);
   }
 };
